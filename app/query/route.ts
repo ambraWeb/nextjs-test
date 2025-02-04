9f288912557a38a337f1500e95474527f32103b5
ambraWeb <ambraduca26@gmail.com>
2025-02-04 15:10:07 +0100

    table drop

--- app/query/route.ts
+++ app/query/route.ts
@@ -2,10 +2,7 @@
  const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
  async function listInvoices() {
  	const data = await sql`
-     SELECT invoices.amount, customers.name
-     FROM invoices
-     JOIN customers ON invoices.customer_id = customers.id
-     WHERE invoices.amount = 666;
+     DROP TABLE tablename
    `;
  	return data;
  }
